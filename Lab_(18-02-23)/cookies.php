af43fd3dd71191e1df9ebfa69027149a45a18947
Albatrosssssss <sadialhuda.aiub@gmail.com>
2023-02-18 12:40:57 +0600

    added

--- Lab_(18-02-23)/cookies.php
+++ Lab_(18-02-23)/cookies.php
@@ -1,3 +1,24 @@
+<?php
+    if(isset($_POST['btn'])){
+        $color= $_POST['color'];
+        $date= $_POST['date'];
+    
+        $error = [];
+        if(empty($_POST['color'])){
+            $error['color']='Select Color';
+        }
+        if(empty($_POST['date'])){
+            $error['date']='Select Date';
+        }
+
+    }
+    if(isset($_POST['bttn'])){
+        if(empty($_POST['date'])){
+            $error['date']='Select Date';
+        }
+        
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,7 +37,7 @@
         <label for="colour">Select a color: </label>
 		<input type="color" name="color" value="#000000"><br><br>
         <label for="" > Expire on </label>
-        <input type="date" name="time"> <br><br>
+        <input type="date" name="date"> <br><br>
 		<input type="submit" name = "btn" value="Set Cookies"|>
 	</form>
     <hr>
