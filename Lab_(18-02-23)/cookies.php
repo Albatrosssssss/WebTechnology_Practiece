fedefecde19e034496e4891c1f9224cf412dab1b
Albatrosssssss <sadialhuda.aiub@gmail.com>
2023-02-18 12:30:57 +0600

    added

--- Lab_(18-02-23)/cookies.php
+++ Lab_(18-02-23)/cookies.php
@@ -9,11 +9,21 @@
 
 	<h1>Cookies Example</h1>
     <p1>Time zone: Asia </p1>
-    <h2>Set Cookie</h2>
+    <hr>
+    <h3>Set Cookies</h3>
+    <hr>
 	<form action="" method="POST">
         <label for="colour">Select a color: </label>
-		<input type="color" name="color" value="#ff0000">
-		<input type="submit">
+		<input type="color" name="color" value="#ff0000"><br><br>
+        <label for="" > Expire on </label>
+        <input type="date" name="time"> <br><br>
+		<input type="submit" name = "brn" value="Set Cookies"|>
+	</form>
+    <hr>
+    <h3>Set Cookies</h3>
+    <hr>
+    <form action="" method="POST">
+        <label for="colour">Select a color: </label>
 	</form>
 
 	<?php 
